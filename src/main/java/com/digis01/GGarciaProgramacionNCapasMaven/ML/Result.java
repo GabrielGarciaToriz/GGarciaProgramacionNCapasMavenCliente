15afffe0ae4a9ea8958c45ab68f1636d58c9bfd0
GabrielGarciaToriz <gabrielgtoriz@hotmail.com>
2026-03-06 13:37:09 -0600

    Clean project

---
+++ src/main/java/com/digis01/GGarciaProgramacionNCapasMaven/ML/Result.java
@@ -0,0 +1,13 @@
+package com.digis01.GGarciaProgramacionNCapasMaven.ML;
+
+import java.util.List;
+
+public class Result <T>{
+    
+    public boolean correct;
+    public String errorMessage;
+    public Exception ex;
+    public Object object;
+    public List<T> objects;
+    
+}
